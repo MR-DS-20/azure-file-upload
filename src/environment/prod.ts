b2b12401a6f4ab39032a485cf74437c6ebcd5f21
Max Antonio Raju <max.a.raju@gmail.com>
2021-06-07 15:15:42 +0100

    debugged production env export

--- src/environment/prod.ts
+++ src/environment/prod.ts
@@ -1,18 +1,23 @@
-export = {
+import { IEnv } from "../interfaces/IEnv"
+
+export const ENV:IEnv ={
     stage: process.env.NODE_ENV,
     port:8082,
-    domain:process.env.DOMAIN,
+    domain:process.env.DOMAIN ?? '',
+    apiPath: '',
+    staticPath: '',
     db:{
         name: '',
         user:'',
         pw: '',
+        account: '',
         uri: (user: string, pw :string, name :string) => {
             return `mongodb+srv://${user}:${pw}@mr-ds-20-ckbes.gcp.mongodb.net/${name}?retryWrites=true&w=majority`
         }
     },
     azureStorage: {
-        STORAGE_ACCOUNT_ACCESS_KEY : process.env.STORAGE_ACCOUNT_ACCESS_KEY,
-        STORAGE_ACCOUNT_NAME: process.env.STORAGE_ACCOUNT_NAME
+        STORAGE_ACCOUNT_ACCESS_KEY : process.env.STORAGE_ACCOUNT_ACCESS_KEY ?? '',
+        STORAGE_ACCOUNT_NAME: process.env.STORAGE_ACCOUNT_NAME ?? ''
         
     }
     
